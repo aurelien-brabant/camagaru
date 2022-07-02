0e84df2ad87ed976c30fdeb3d2528f87692c6775
Aurélien Brabant <abrabant@student.42.fr>
2022-07-02 21:34:37 +0200

    Add ability to take photo

--- src/public/javascript/camera.js
+++ src/public/javascript/camera.js
@@ -7,6 +7,8 @@
 (async () => {
 	const VIDEO_OUTPUT_ID = 'camera-video';
 	const CAMERA_PHOTO_TRIGGER_ID = 'camera-photo-trigger';
+    const CAMERA_HIDDEN_CANVAS_ID = 'camera-hidden-canvas';
+    const HIDDEN_CANVAS_PHOTO_ID = 'hidden-canvas-photo';
 
 	/**
 	 * @type {HTMLVideoElement | null}
@@ -18,10 +20,38 @@
 	 */
 	const cameraPhotoTrigger = document.getElementById(CAMERA_PHOTO_TRIGGER_ID);
 
+    /**
+     * @type {HTMLCanvasElement | null}
+     */
+    const hiddenCanvas = document.getElementById(CAMERA_HIDDEN_CANVAS_ID);
+
+    /**
+     * @type {HTMLImageElement | null}
+     */
+    const hiddenCanvasPhoto = document.getElementById(HIDDEN_CANVAS_PHOTO_ID);
+
 	/**
 	 * @type {MediaStream | null}
 	 */
 	let stream = null;
+
+    const takePicture = () => {
+        if (!hiddenCanvas) {
+            console.error(`Could not find canvas with id ${CAMERA_HIDDEN_CANVAS_ID}`)
+
+            return ;
+        }
+
+        const canvasContext = hiddenCanvas.getContext('2d');
+
+        hiddenCanvas.width = video.videoWidth;
+        hiddenCanvas.height = video.videoHeight;
+        canvasContext.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+
+        const data = hiddenCanvas.toDataURL('image/png');
+
+        hiddenCanvasPhoto.setAttribute('src', data);
+    } 
 
 	if (video) {
 		try {
@@ -38,7 +68,7 @@
 
 	if (cameraPhotoTrigger) {
 		cameraPhotoTrigger.addEventListener('click', () => {
-			alert('CHEESE!');
+            takePicture();
 		});
 	} else {
 		console.error(`Could not find photo trigger with id ${CAMERA_PHOTO_TRIGGER_ID}`);
